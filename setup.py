8d31d09bf58313c0b583bd3512c05031cf7dcbd2
David Danier <david.danier@team23.de>
2012-12-09 13:40:55 +0100

    Added setup.py and cleanup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django_i18n_attrhelpers",
+    version = "0.1.0",
+    description = 'Helpers for accessing ..._$LANG-fields',
+    author = 'David Danier',
+    author_email = 'david.danier@team23.de',
+    url = 'https://github.com/ddanier/django_i18n_attrhelpers',
+    #long_description=open('README.rst', 'r').read(),
+    packages = [
+        'django_i18n_attrhelpers',
+    ],
+    package_data = {
+        'django_i18n_attrhelpers': ['locale/*/LC_MESSAGES/*'],
+    },
+    requires = [
+        'django(>=1.4)',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
+
